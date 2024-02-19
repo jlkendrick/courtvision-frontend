f44b64a39522a6974efcfd830d94be153f7a1041
jameslk3 <kendrickj5@yahoo.com>
2024-02-19 03:54:56 -0600

    Added Header component and background images and logos

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -4,24 +4,27 @@ import ListFeatures from './ListFeatures'
 import Home from '../pages/Home'
 import Stoptz from '../pages/Stoptz'
 import Header from "../components/Header";
+import "../styles/App.css"
 
 export default function App() {
 
   return (
     <>
 
-    <Header />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/stoptz" element={<Stoptz />} />
-      </Routes>
-    </BrowserRouter>
-    <div>
-      <LeagueInfo />
-    </div>
-    <div>
-      <ListFeatures />
+    <div className="appBackground">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stoptz" element={<Stoptz />} />
+        </Routes>
+      </BrowserRouter>
+      <div>
+        <LeagueInfo />
+      </div>
+      <div>
+        <ListFeatures />
+      </div>
     </div>
 
     </>
--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,15 +1,20 @@
-import Logo from '../public/Logo_transparent_outer.png';
-import "../styles/App.css"
+import Logo from '../public/FancyLogo.png';
+import "../styles/Header.css"
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-light bg-custom">
-      <a className="navbar-brand" href="#">
-        <img src={Logo} width="200" height="150" alt="logo" />
-        Court Visionaries
-      </a>
-      {/* Other navbar content can be added here */}
-    </nav>
+    <header className="header">
+
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+
+      <div className="title">
+        <h2></h2> 
+        <h1>Court Visionaries</h1> 
+      </div>
+
+    </header>
   );
 }
   
