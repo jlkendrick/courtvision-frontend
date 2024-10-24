33113065a00b168dafb6486f8f9bafff47ec168e
James <kendrickj5@yahoo.com>
2024-10-23 21:18:17 -0500

    Refactor ETL route to use path and fs modules

--- app/src/app/api/data/etl/update-fpts/route.tsx
+++ app/src/app/api/data/etl/update-fpts/route.tsx
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { DATABSE_API_ENDPOPINT } from "@/endpoints";
-import * as fs from "fs";
+import path from 'path';
+import fs from 'fs';
 
-// API route to intiate the POST the new day's data to the database and retrieve that updated data  --------------------------------------------
+// API route to intiate the POST the new day's data to the database  --------------------------------------------
 export async function POST(request: NextRequest): Promise<NextResponse> {
   console.log("POST request to /api/data/etl/update-fpts");
   const CRON_TOKEN = process.env.CRON_TOKEN;
@@ -25,17 +26,40 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     },
     body: JSON.stringify({ cron_token: token }),
   });
+
   if (!response.ok) {
-    return NextResponse.json({ error: "Error occurred updating fpts in database." }, { status: 400 });
+    console.log("Error starting ETL process");
+    return NextResponse.json({ error: "Error occurred starting ETL process." }, { status: 400 });
   }
 
-  // Update the JSON file with the new data
-  const data = await response.json();
+  return NextResponse.json({ message: "Successfully started ETL process, check frontend or public route to see updated data." });
+}
+
+// API route to listen for the response from the backend server for when it is done with the ETL process  --------------------------------------------
+export async function PUT(request: NextRequest): Promise<NextResponse> {
+  console.log("PUT request to /api/data/etl/update-fpts");
+  const CRON_TOKEN = process.env.CRON_TOKEN;
+  const token = request.headers.get("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    console.log("No authorization token");
+    return NextResponse.json({ error: "No authorization token" }, { status: 400 });
+  }
+
+  if (token !== CRON_TOKEN) {
+    console.log("Invalid token");
+    return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+  }
+
+  const body = await request.json();
+  const data = body.data;
   try {
-    fs.writeFileSync("../../../../../../public/standings-data/fpts.json", JSON.stringify(data, null, 2));
+    const filePath = path.resolve(process.cwd(), 'public/standings-data/fpts.json');
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    console.log("Data successfully written to file");
+    return NextResponse.json({ message: "Data successfully written to file." });
   } catch (error) {
     console.log("Error writing to file: ", error);
+    return NextResponse.json({ error: "Failed to write data to file." }, { status: 500 });
   }
-
-  return NextResponse.json("Successfully updated fpts in database, check frontend to see updated standings.");
 }
